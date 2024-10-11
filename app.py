7f7eec0c6f72f01f3ec2240e7fa355ba6acebe16
djiawou <amourakowakou@gmail.com>
2024-10-11 13:40:11 +0200

    add and delete

--- app.py
+++ app.py
@@ -163,4 +163,4 @@ def update_task_status(task_id):
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
